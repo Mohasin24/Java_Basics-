384cd652cdec71a2053403975a56ff4a6c9fd3d5
Mohasin Patel <mohasinpatel313@gmail.com>
2023-02-02 14:54:52 +0530

    Add files via upload

---
+++ 02_DataTypes/check_size_range_of_dataType.java
@@ -0,0 +1,20 @@
+
+import java.lang.*;
+
+public class check_size_range_of_dataType 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("int MIN :"+Integer.MIN_VALUE);    
+        System.out.println("int MAX :"+Integer.MAX_VALUE);    
+        System.out.println("int size :"+Integer.BYTES);    
+        System.out.println("byte size :"+Byte.BYTES);    
+        System.out.println("short size :"+Short.BYTES);    
+        System.out.println("long size :"+Long.BYTES);    
+        System.out.println("float size:"+Float.BYTES);    
+        System.out.println("double size:"+Double.BYTES);    
+        System.out.println("character size:"+Character.BYTES);    
+        
+        
+    }    
+}
---
+++ 02_DataTypes/variables.java
@@ -0,0 +1,13 @@
+/* 
+- Variable initialisation is compulsory in java
+
+- 
+*/
+
+public class variables
+{
+    public static void main(String[] args) {
+        float f = 2.03f;
+
+    }
+}
